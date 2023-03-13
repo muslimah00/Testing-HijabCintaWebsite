99fd6b37dfc7ec821164e340f399608fbd6fe4c5
muslimah00 <muslimah.mbs@gmail.com>
2023-03-13 17:48:14 +0700

    testing login

---
+++ TestHijabCintaWebsite/Object Repository/tc01 - Login/tc01 - Login using registered account/Page_Hijab Cinta/a_Login.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Login</name>
+   <tag></tag>
+   <elementGuidId>b1ad95bc-ed91-4c99-9f86-f586fc7da812</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Login')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>ccfdcc27-a9df-4f57-b11a-1bf4979bec95</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://hijabcinta.online/login</value>
+      <webElementGuid>8a64d4dc-ae94-482f-bab4-39f5f0b98e96</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Login </value>
+      <webElementGuid>e37bc776-ee7f-4527-a0d6-903ca6c60b32</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;goto-here&quot;]/div[@class=&quot;py-1 bg-black&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row no-gutters d-flex align-items-start align-items-center px-md-0&quot;]/div[@class=&quot;col-lg-12 d-block&quot;]/div[@class=&quot;row d-flex&quot;]/div[@class=&quot;col-md-2 pr-4 d-flex topper align-items-center text-lg-right&quot;]/a[1]</value>
+      <webElementGuid>4c4da6ab-c04a-4a2f-90f8-d3f2bf5b3f3f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Login')]</value>
+      <webElementGuid>aa393ad2-2e48-4267-bb78-4f086810aaf3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='hijabcinta99@gmail.com'])[1]/following::a[1]</value>
+      <webElementGuid>eb6a7cb5-1ea4-4f48-a97f-ff8cad7ccd44</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Register'])[1]/preceding::a[4]</value>
+      <webElementGuid>e9b8df95-2a1e-4387-a737-85c466751d86</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Login']/parent::*</value>
+      <webElementGuid>1d23d302-53e7-4785-a558-c24fb50898fb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://hijabcinta.online/login')]</value>
+      <webElementGuid>9e6a9587-68aa-4d03-9d60-fc08e5e31666</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a</value>
+      <webElementGuid>3ccba482-5fdf-43b4-ac53-755e23889308</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://hijabcinta.online/login' and (text() = 'Login ' or . = 'Login ')]</value>
+      <webElementGuid>09570568-a091-4f8b-b5a1-ca12c09193fe</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
